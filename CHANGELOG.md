bc892bf9a4af40f1ce1a2d9ec84ae59382de5d35
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-17 11:03:57 +0300

    Development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# _____ Change Log
+# Mailery Widget Datepicker Assets Change Log
 
 ## 1.0.0 under development
 
---
+++ src/components/Datepicker.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ui-datepicker">
+    <input type="hidden" :name="name" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ui-datepicker',
+    props: {
+      name: String
+    }
+  }
+</script>
